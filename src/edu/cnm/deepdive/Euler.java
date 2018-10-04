f926a05d5e570b799ad608dd391ab8e1be818a0f
Eve <evejrobles@icloud.com>
2018-10-04 06:18:18 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/Euler.java
@@ -0,0 +1,26 @@
+package edu.cnm.deepdive;
+
+public class Euler {
+
+
+  public static void main(String[] args) {
+    long sum = 0;
+
+    for (long i = 1; i < 1000; i++) {
+
+      if (i % 3 == 0) {
+        sum += i;
+
+      } else if (i % 5 == 0) {
+        sum += i;
+
+      }
+
+
+      }
+
+      System.out.println(sum);
+
+
+    }
+  }
